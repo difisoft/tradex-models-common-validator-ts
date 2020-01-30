de68053b6828d8bf4eda4bbda1a99709088bc47c
schema-generator <developer@techx.vn>
2020-01-30 15:30:43 +0700

    schema genarator by buildNumber:

---
+++ build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const baseRequest_1 = require("./baseRequest");
+exports.baseRequestSchema = baseRequest_1.baseRequestSchema;
+exports.baseRequestValidator = baseRequest_1.baseRequestValidator;
+const requestHeader_1 = require("./requestHeader");
+exports.requestHeaderSchema = requestHeader_1.requestHeaderSchema;
+exports.requestHeaderValidator = requestHeader_1.requestHeaderValidator;
+const token_1 = require("./token");
+exports.tokenSchema = token_1.tokenSchema;
+exports.tokenValidator = token_1.tokenValidator;
+const userData_1 = require("./userData");
+exports.userDataSchema = userData_1.userDataSchema;
+exports.userDataValidator = userData_1.userDataValidator;
+//# sourceMappingURL=index.js.map
